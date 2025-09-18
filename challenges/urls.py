fac8a8b2f13968fe38142ba87fd578948b18adf5
Salvatore Pappalardo <pappalardo.dev@gmail.com>
2025-09-18 09:48:24 +0200

    practice with urls and views

---
+++ challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+'we are doing a url config'
+urlpatterns = [
+    path("gennaio",views.gennaioFunzione),
+    path("febbraio",views.febbraioFunzione)
+]
