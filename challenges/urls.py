b34e2a794f0a162ade736bedf1f1d5991927bcd8
Salvatore Pappalardo <pappalardo.dev@gmail.com>
2025-09-22 20:09:44 +0200

    dynamic urls functions in views.py

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 'we are doing a url config'
+
+'''
 urlpatterns = [
     path("gennaio",views.gennaioFunzione),
     path("febbraio",views.febbraioFunzione)
+]
+'''
+
+urlpatterns = [
+    path("<mese>",views.challengeDelMese)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+'''
 def gennaioFunzione(request):
       return HttpResponse("siamo su gennaio")
 
 def febbraioFunzione(request):
       return HttpResponse("siamo su febbraio")
+'''
+
+def challengeDelMese(request, mese):
+    variabileTesto=None
+
+    if mese == "gennaio":
+        variabileTesto="Siamo a Gennaio"
+    elif mese == "febbraio":
+        variabileTesto="Siamo a Febbraio"
+    elif mese =="marzo":
+        variabileTesto="Siamo a Marzo"
+    else:
+        return HttpResponseNotFound("Questo mese non esiste")
+    return HttpResponse(variabileTesto)
